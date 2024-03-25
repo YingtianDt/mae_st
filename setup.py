9dbdbd5fcb75724f2b37609042c5b668af2353c3
YingtianDt <yingtian.tang@epfl.ch>
2024-03-25 19:34:21 +0100

    setup

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import find_packages, setup
 # minimal setup for brainscore
 setup(
     name='mae_st',
-    packages=find_packages(include=['mae_st'])
+    packages=find_packages()
 )
